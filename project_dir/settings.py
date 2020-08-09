b619d7a603b45126ad18b1e69089a72739a1eec3
zewei.li <zewei.li@mei1.com>
2020-08-09 08:53:15 +0800

    框架可配置化，实现项目配置文件默认覆盖框架配置文件

---
+++ project_dir/settings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : willi
+# @Email   : willi168@163.com
+# @Description:
+
+DEFAULT_LOG_FILENAME = '日志.log'
---
+++ scrapy_simple/conf/settings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : willi
+# @Email   : willi168@163.com
+# @Description:
+from .default_settings import *
+from settings import *      # 这里导入的settings，是项目文件夹的settings文件。利用的是import导包的顺序
